645aaab8e4ad917e70b8915a2d1189ca89185183
LucasPradellam <lucas.pradella@hotmail.com>
2019-04-27 13:14:25 -0300

    add projeto inicial

---
+++ src/main/java/br/com/pradella/PradellaEstudiesAmqpApplication.java
@@ -0,0 +1,13 @@
+package br.com.pradella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PradellaEstudiesAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PradellaEstudiesAmqpApplication.class, args);
+	}
+
+}
